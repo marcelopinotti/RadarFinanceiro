e343ac360d89a1fe1b6fbac08a9aabc76644d6a9
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-05 16:33:27 -0300

    refactor: simplify CRUDService interface by removing generic Id type

--- src/main/java/com/marcelopinotti/radar_financeiro/domain/service/CRUDService.java
+++ src/main/java/com/marcelopinotti/radar_financeiro/domain/service/CRUDService.java
@@ -3,16 +3,16 @@ package com.marcelopinotti.radar_financeiro.domain.service;
 
 import java.util.List;
 
-public interface CRUDService<Request, Response, Id> {
+public interface CRUDService<Request, Response> {
 
     Response criar(Request dto);
 
-    List<Request> obterTodos();
+    List<Response> obterTodos();
 
-    Response buscarPorId(Id id);
+    Response buscarPorId(long id);
 
-    Response atualizar(Id id, Request dto);
+    Response atualizar(long id, Request dto);
 
-    void deletar(Id id);
+    void deletar(long id);
 
 }
