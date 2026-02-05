d57b398c76ca0c93a271ec582d3ead4138896abb
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-05 12:28:06 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/marcelopinotti/radar_financeiro/RadarFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.marcelopinotti.radar_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelopinotti/radar_financeiro/RadarFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelopinotti.radar_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
