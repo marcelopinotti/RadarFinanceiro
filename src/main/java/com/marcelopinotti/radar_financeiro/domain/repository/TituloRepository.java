70a53e11736c3b0210e7c1482d009f0ae593d29e
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-05 15:27:04 -0300

    feat: add repository interfaces for CentroDeCusto, Titulo, and Usuario

---
+++ src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/TituloRepository.java
@@ -0,0 +1,7 @@
+package com.marcelopinotti.radar_financeiro.domain.repository;
+
+import com.marcelopinotti.radar_financeiro.domain.model.Titulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TituloRepository extends JpaRepository<Titulo,Long> {
+}
---
+++ src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.marcelopinotti.radar_financeiro.domain.repository;
+
+import com.marcelopinotti.radar_financeiro.domain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    List<Usuario> findByEmail(String email);
+}
