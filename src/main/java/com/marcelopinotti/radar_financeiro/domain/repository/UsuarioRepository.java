3ef892894cf7ce3d5f360c2c38464b5e5af961b0
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-08 18:22:20 -0300

    refactor: update UsuarioRepository to return Optional for findByEmail

--- src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/UsuarioRepository.java
+++ src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/UsuarioRepository.java
@@ -3,9 +3,10 @@ package com.marcelopinotti.radar_financeiro.domain.repository;
 import com.marcelopinotti.radar_financeiro.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    List<Usuario> findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
