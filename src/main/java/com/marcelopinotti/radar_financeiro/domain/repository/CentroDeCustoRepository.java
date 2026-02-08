913b1d1e9d27d29e0e1b2b1358813aafd7a51403
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-08 15:40:29 -0300

    refactor: remove unused CentroDeCustoRepository and TituloRepository interfaces

--- src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/CentroDeCustoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marcelopinotti.radar_financeiro.domain.repository;
-
-import com.marcelopinotti.radar_financeiro.domain.model.CentroDeCusto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CentroDeCustoRepository  extends JpaRepository<CentroDeCusto, Long> {
-}
--- src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/TituloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marcelopinotti.radar_financeiro.domain.repository;
-
-import com.marcelopinotti.radar_financeiro.domain.model.Titulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TituloRepository extends JpaRepository<Titulo,Long> {
-}
