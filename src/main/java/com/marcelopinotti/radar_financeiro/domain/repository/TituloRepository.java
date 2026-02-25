2e7f8e5b909baa0898b191295d9c1fef554c5579
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-25 15:37:12 -0300

    refractor: add method to fetch Titulos by due date range in TituloRepository

--- src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/TituloRepository.java
+++ src/main/java/com/marcelopinotti/radar_financeiro/domain/repository/TituloRepository.java
@@ -2,6 +2,21 @@ package com.marcelopinotti.radar_financeiro.domain.repository;
 
 import com.marcelopinotti.radar_financeiro.domain.model.Titulo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface TituloRepository extends JpaRepository<Titulo, Long> {
+
+    @Query("""
+       SELECT t
+       FROM Titulo t
+       WHERE t.dataVencimento BETWEEN :periodoInicial AND :periodoFinal
+       """)
+    List<Titulo> obterPorDataVencimento(
+            @Param("periodoInicial") Date periodoInicial,
+            @Param("periodoFinal") Date periodoFinal
+    );
 }
